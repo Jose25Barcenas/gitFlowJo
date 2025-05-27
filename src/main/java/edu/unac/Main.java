6c70c523df38565381fea76e456f223c23c7470f
Jose Barcenas <josea.barcenasg@unac.edu.co>
2025-05-26 20:53:06 -0500

    comentario funcionalidad 2

--- src/main/java/edu/unac/Main.java
+++ src/main/java/edu/unac/Main.java
@@ -1,5 +1,6 @@
 package edu.unac;
 
+// Comentario de la funcionalidad 1 corregido
 public class Main {
     public static void main(String[] args) {
         System.out.println("Proyecto GitFlowJose");
