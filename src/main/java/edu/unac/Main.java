ecd415d6e9905c7cae287791b6c0498fa2dc93a2
Jose Barcenas <josea.barcenasg@unac.edu.co>
2025-05-27 00:02:37 -0500

    fix: correccion en comentario

--- src/main/java/edu/unac/Main.java
+++ src/main/java/edu/unac/Main.java
@@ -1,8 +1,9 @@
 package edu.unac;
 
-// Comentario de la funcionalidad 1 corregido
 public class Main {
     public static void main(String[] args) {
+        // Esto es un comentario de la funcionalidad 1 corregido
         System.out.println("Proyecto GitFlowJose");
+
     }
 }
